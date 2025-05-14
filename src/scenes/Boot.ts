fe50db71ffee0269615d83c1bc11f524d7cf68a2
s-cittadin <sofia.c11@aluno.ifsc.edu.br>
2025-05-14 18:24:12 +0000

    Primeira imagem do jogo

--- src/scenes/Boot.ts
+++ src/scenes/Boot.ts
@@ -1,7 +1,15 @@
-import { Scene } from 'phaser';
+import { Scene } from "phaser";
 
 export class Boot extends Scene {
-    constructor() {
-        super({ key: 'Boot' });
-    }
+  constructor() {
+    super({ key: "Boot" });
+  }
+
+  preload() {
+    this.load.image("dungeon", "assets/dungeon.png");
+  }
+
+  create() {
+    this.add.image(400, 225, "dungeon");
+  }
 }
