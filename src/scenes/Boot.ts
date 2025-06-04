a60fab1252ebd4b4c0d8e8fc3857eabc73f0513b
Gabe-Jeremas <gabriel.jc2006@aluno.ifsc.edu.br>
2025-06-04 18:21:11 +0000

    INICIAR CAÇA AO TESOURO

--- src/scenes/Boot.ts
+++ src/scenes/Boot.ts
@@ -4,4 +4,12 @@ export class Boot extends Scene {
     constructor() {
         super({ key: "Boot" });
     }
+    preload() {
+        // Load assets here
+        this.load.image("img", "assets/img.png");
+    }
+    create() {
+        // Start the next scene
+        this.add.image(400, 225, "img");
+    }
 }
